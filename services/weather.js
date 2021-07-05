debe3176d5bd1c6a919053651350d21affaeba14
BudakHasan <budak.hasan.apc@gmail.com>
2021-07-05 18:43:14 +0300

    # Change: Add fallback value for api key on weather service functions

--- services/weather.js
+++ services/weather.js
@@ -1,18 +1,19 @@
-export const BASE_API_URL = `https://api.openweathermap.org/data/2.5/weather?appid=${process.env.API_KEY}&units=metric`
+const API_KEY = `${process.env.API_KEY || 'b9baca56aa96c2081319148084846a37'}`
+export const BASE_API_URL = `https://api.openweathermap.org/data/2.5/weather`
+export const API_URL = `${BASE_API_URL}?appid=${API_KEY}&units=metric`
+
 export async function getForecastByCoordinate(
   latitude,
   longitude,
   lang = 'en'
 ) {
   const response = await fetch(
-    `${BASE_API_URL}&latitude=${latitude}&longitude=${longitude}&lang=${lang}`
+    `${API_URL}&latitude=${latitude}&longitude=${longitude}&lang=${lang}`
   )
-
   return response.json()
 }
 
 export async function getForecastByQuery(query, lang = 'en') {
-  const response = await fetch(`${BASE_API_URL}&q=${query}&lang=${lang}`)
-
+  const response = await fetch(`${API_URL}&q=${query}&lang=${lang}`)
   return response.json()
 }
